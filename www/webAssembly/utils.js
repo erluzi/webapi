913df99c99d81baf2694cbe610599c559175d2ef
erluzi <747877078@qq.com>
2020-05-22 16:29:16 +0800

    fix: code format

--- www/webAssembly/utils.js
+++ www/webAssembly/utils.js
@@ -1,34 +1,34 @@
-function loadWebAssembly(filename, imports={}, type='wasm') {
-    if(typeof WebAssembly !== 'undefined' && type === 'wasm'){
-        return fetch(filename)
-            .then(response => response.arrayBuffer())
-            .then(buffer => WebAssembly.compile(buffer))
-            .then(module => {
-                imports.env = imports.env || {};
-                Object.assign(imports.env, {
-                    memoryBase: 0,
-                    tableBase: 0,
-                    memory: new WebAssembly.Memory({initial: 256, maximum: 256}),
-                    table: new WebAssembly.Table({initial: 0, maximum: 0, element: 'anyfunc'})
-                });
-                return new WebAssembly.Instance(module, imports)
-            })
-    }else{
-        filename = filename.replace(/\.wasm$/, '.js');
-        return new Promise((resolve, reject) => {
-            let instance = {
-                exports: function () {
-
-                }
-            }
-            resolve(instance)
+function loadWebAssembly(filename, imports = {}, type = 'wasm') {
+  if (typeof WebAssembly !== 'undefined' && type === 'wasm') {
+    return fetch(filename)
+        .then(response => response.arrayBuffer())
+        .then(buffer => WebAssembly.compile(buffer))
+        .then(module => {
+          imports.env = imports.env || {}
+          Object.assign(imports.env, {
+            memoryBase: 0,
+            tableBase: 0,
+            memory: new WebAssembly.Memory({initial: 256, maximum: 256}),
+            table: new WebAssembly.Table({initial: 0, maximum: 0, element: 'anyfunc'}),
+          })
+          return new WebAssembly.Instance(module, imports)
         })
-    }
+  } else {
+    filename = filename.replace(/\.wasm$/, '.js')
+    return new Promise((resolve, reject) => {
+      let instance = {
+        exports: function () {
+
+        },
+      }
+      resolve(instance)
+    })
+  }
 }
 
 function test() {
-    loadWebAssembly('a.wasm').then(instance => {
-        let func = instance.exports._func;
-        func();
-    })
+  loadWebAssembly('a.wasm').then(instance => {
+    let func = instance.exports._func
+    func()
+  })
 }
